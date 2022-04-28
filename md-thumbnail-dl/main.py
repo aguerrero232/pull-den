47e1736147564d545abefe35864890f38167022a
Hunter C Long <90282803+Hunter-Long112@users.noreply.github.com>
2022-04-28 00:41:46 -0500

    cleaning up code

--- md-thumbnail-dl/main.py
+++ md-thumbnail-dl/main.py
@@ -16,17 +16,15 @@ def md_thumbnail_dl(event, context):
      vid_url = pubsub_message
 
      ydl_opts={
-               'outtmpl':'/tmp/%(id)s.%(ext)s',
-               #'format':'mp4'
-               #'outtmpl':'/tmp/%(id)s.mp4'
+               'outtmpl':'/tmp/%(id)s.%(ext)s'
           }
      with yt_dlp.YoutubeDL(ydl_opts) as ytdlp:
           vid_info = ytdlp.extract_info(vid_url, download=False)
           vid_id = vid_info['id']
           vid_ext = vid_info['ext']
           
-          thumbnail_filename = f'/tmp/{vid_id}.{vid_ext}'
-          urllib.request.urlretrieve(vid_info['thumbnail'], thumbnail_filename)
+     thumbnail_filename = f'/tmp/{vid_id}.{vid_ext}'
+     urllib.request.urlretrieve(vid_info['thumbnail'], thumbnail_filename)
 
      image = Image.open(thumbnail_filename).convert('RGB')
      thumbnail_jpg_version = f'/tmp/{vid_id}.jpg'
@@ -36,5 +34,3 @@ def md_thumbnail_dl(event, context):
      bucket = storage_client.get_bucket('md-thumbnail')
      blob = bucket.blob(vid_id)
      blob.upload_from_filename(thumbnail_jpg_version)
-
-

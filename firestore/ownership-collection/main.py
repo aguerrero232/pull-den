fbea4be2641ef0c39f648f837fb6536c6752202c
ViswaB <viswa.bhargavi.2000@gmail.com>
2022-05-03 21:00:21 -0500

    ownership collection cloud func

---
+++ firestore/ownership-collection/main.py
@@ -0,0 +1,37 @@
+from google.cloud import firestore
+db=firestore.Client("cs4843-youtube-dl")
+
+def entryPoint(request):
+  request_json = request.get_json()
+  try:
+    if request and 'id' in request_json:
+      doc_ref = db.collection("OWNERSHIP").document(request_json['id'])
+      doc = doc_ref.get()
+      if doc.exists:
+        data = doc.to_dict()
+        for key, value in data.items():
+          if key == 'videos':
+            videoList = value
+            if request_json['vidID'] not in videoList:
+              vid_ref = db.collection("VIDEOS").document(request_json['vidID'])
+              vidDoc = vid_ref.get()
+              if not vidDoc.exists:
+                video_ref.set({
+                  "vidID":request_json['vidID'],
+                  "title":request_json['title'],
+                  "channelID":request_json['channelID'],
+                  "GCSVideo":request_json['GCSVideo']
+                  })
+              videoList.append(request_json['vidID'])
+              doc.set({
+                  "rootUserID":request_json['id'],
+                  "Videos":videoList
+              })
+      return "Added to Ownership collection"
+    else:
+      return "Failed if statement"
+    return request_json['id']
+    
+  except BaseException as e:
+    return str(e)
+
